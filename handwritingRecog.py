68481421f6a66cc851abb5f36905866d383b3b69
Shalini Bhandari <76897566+shalini-bhandari@users.noreply.github.com>
2025-04-25 07:26:31 +0530

    Add files via upload

--- handwritingRecog.py
+++ handwritingRecog.py
@@ -104,15 +104,15 @@ for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
 pred = neural_net(x_test)
 print("Test Accuracy: %f" % accuracy(pred, y_test))
 
-# n_images = 200
-# test_images = x_test[:n_images]
-# test_labels = y_test[:n_images]
-# predictions = neural_net(test_images)
+n_images = 200
+test_images = x_test[:n_images]
+test_labels = y_test[:n_images]
+predictions = neural_net(test_images)
 
-# for i in range(n_images):
-#     model_prediction = np.argmax(predictions.numpy()[i])
-#     if(model_prediction != test_labels[i]):
-#         plt.imshow(np.reshape(test_images[i], [28, 28]), cmap = 'gray_r')
-#         plt.show()
-#         print("Original Labels: %i" % test_labels[i])
-#         print("Model prediction: %i" % model_prediction)
+for i in range(n_images):
+    model_prediction = np.argmax(predictions.numpy()[i])
+    if(model_prediction != test_labels[i]):
+        plt.imshow(np.reshape(test_images[i], [28, 28]), cmap = 'gray_r')
+        plt.show()
+        print("Original Labels: %i" % test_labels[i])
+        print("Model prediction: %i" % model_prediction)
